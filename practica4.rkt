b296a04bd30c27973d298426ccf0c47f141af232
Alitares27 <aldair2795@hotmail.com>
2025-05-04 20:40:09 -0300

    Practica4, 'avance de ejercicio 6'

--- practica4.rkt
+++ practica4.rkt
@@ -114,8 +114,73 @@
   [on-key movement]
   [on-mouse mouse-handler])
 
+; Ej6
 
 
+(define MOSCA
+  (bitmap "C:/Users/aldai/OneDrive/UNR/Racket/fly.png"))
+
+(define ALT 500)
+(define ANC 500)
+(define MOSCANC 50)
+(define AIRE(empty-scene ANC ALT "skyblue"))
+(define DELTAI 21)
+(define GAMMA 30)
+(define ESTADO-FINAL (make-posn -1 -1))
+
+(define-struct estado (pos dx dy))
+
+(define POSICION (make-estado (make-posn (/ ANC 2)(/ ALT 2))
+  (- (random DELTAI)10)
+  (- (random DELTAI)10)))
+
+(define (espacio m)
+  (if (equal? (estado-pos m)ESTADO-FINAL)
+      (place-image (text "MOSCA ATRAPADA" 30 "red")(/ ANC 2)(/ ALT 2)
+                   (empty-scene ANC ALT))
+      (place-image MOSCA
+                   (posn-x (estado-pos m))
+                   (posn-y (estado-pos m))
+                   (empty-scene ANC ALT))))
+
+(define (vuelo m)
+  (let* ([dx (estado-dx m)]
+         [dy (estado-dy m)]
+         [p (estado-pos m)]
+         [x (+ (posn-x p) dx)]
+         [y (+ (posn-y p) dy)]
+         [nx (cond [(< x 0) 0]
+                   [(> x (- ANC MOSCANC)) (- ANC MOSCANC)]
+                   [else x])]
+         [ny (cond [(< y 0) 0]
+                   [(> y (- ALT MOSCANC)) (- ALT MOSCANC)]
+                   [else y])]
+         [ndx (if (or (< x 0) (> x (- ANC MOSCANC))) (- dx) dx)]
+         [ndy (if (or (< y 0) (> y (- ALT MOSCANC))) (- dy) dy)])
+    (make-estado (make-posn nx ny) ndx ndy)))
+
+
+(define (distance p1 p2)
+  (sqrt (+(sqr (- (posn-x p1)(posn-x p2)))
+          (sqr (- (posn-y p1)(posn-y p2))))))
+
+
+(define (atrapar m x y e)
+  (if (string=? e "button-down")
+      (if(< (distance (estado-pos m)(make-posn x y)) GAMMA)
+         (make-estado ESTADO-FINAL 0 0)
+         m)
+      m)) 
+
+(define (muerta m)
+  (equal? (estado-pos m) ESTADO-FINAL))
+
+(big-bang POSICION
+  [to-draw espacio]
+  [on-tick vuelo 0.1]
+  [on-mouse atrapar]
+  [stop-when muerta espacio]
+)
 
 
 
